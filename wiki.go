1cf93ed92047b7db3befdd3ab4e446a85384ad6c
Tomoki Oke(LITALICO) <tomoki.oke@litalico.co.jp>
2021-10-01 17:54:02 +0900

    :heavy_minus_sign:remove main

--- wiki.go
+++ wiki.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 )
 
@@ -22,11 +21,4 @@ func loadPage(title string) (*Page, error) {
 		return nil, err
 	}
 	return &Page{Title: title, Body: body}, nil
-}
-
-func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
 }
